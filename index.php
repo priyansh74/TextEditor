4d693a398c9dc58ffa37e99944fe026ab817dade
shantanuprasadbtech <prasad.shantanu11@gmail.com>
2024-04-16 11:44:41 +0530

    Logout button added

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ if ($result && mysqli_num_rows($result) > 0) {
 <head>
   <meta charset="utf-8">
   <title>Text Editor</title>
+  <a href="logout.php" class="btn btn-warning">Logout</a>
   <link rel="icon" href="icon.png">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
