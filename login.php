8da51ff377baa260d0296c25918e833003d46371
shantanuprasadbtech <prasad.shantanu11@gmail.com>
2024-04-16 11:57:37 +0530

    Previous

--- login.php
+++ login.php
@@ -41,10 +41,10 @@ if (isset($_SESSION["user"])) {
         ?>
       <form action="login.php" method="post">
         <div class="form-group">
-            <input type="email" placeholder="Enter Email:" name="email" class="form-control">
+            <input type="email" placeholder="Enter Email" name="email" class="form-control">
         </div>
         <div class="form-group">
-            <input type="password" placeholder="Enter Password:" name="password" class="form-control">
+            <input type="password" placeholder="Enter Password" name="password" class="form-control">
         </div>
         <div class="form-btn">
             <input type="submit" value="Login" name="login" class="btn btn-primary">
